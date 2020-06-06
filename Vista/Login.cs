f046833db7c3690ad4b107a02dbddd8158e80681
Nicolas <nicolas.borjardila11@gmail.com>
2020-06-06 19:36:27 -0400

    Update Login.cs

--- Vista/Login.cs
+++ Vista/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Vista
 {
-    public partial class Form1 : Form
+    public partial class _ventanaLogin : Form
     {
-        public Form1()
+        public _ventanaLogin()
         {
             InitializeComponent();
         }
