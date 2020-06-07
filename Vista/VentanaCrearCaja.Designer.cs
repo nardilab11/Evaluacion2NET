f3cef2ceff098a00e3bc895bbbdb1bb458b74c21
Nicolas <nicolas.borjardila11@gmail.com>
2020-06-07 12:54:51 -0400

    Create VentanaCrearCaja.Designer.cs

---
+++ Vista/VentanaCrearCaja.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Evaluacion2NET.Vista
+{
+    partial class _ventanaCrearCaja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_ventanaCrearCaja));
+            this._botonCancelarCaja = new System.Windows.Forms.Button();
+            this._botonAgregarCaja = new System.Windows.Forms.Button();
+            this._labelNombreCajero = new System.Windows.Forms.Label();
+            this._labelApellidoCajero = new System.Windows.Forms.Label();
+            this._labelRutCajero = new System.Windows.Forms.Label();
+            this._textboxNombreCajero = new System.Windows.Forms.TextBox();
+            this._textboxApellidoCajero = new System.Windows.Forms.TextBox();
+            this._textboxRutCajero = new System.Windows.Forms.TextBox();
+            this._textboxNumCaja = new System.Windows.Forms.TextBox();
+            this._labelNumCaja = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _botonCancelarCaja
+            // 
+            this._botonCancelarCaja.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._botonCancelarCaja.Location = new System.Drawing.Point(425, 242);
+            this._botonCancelarCaja.Name = "_botonCancelarCaja";
+            this._botonCancelarCaja.Size = new System.Drawing.Size(117, 42);
+            this._botonCancelarCaja.TabIndex = 0;
+            this._botonCancelarCaja.Text = "Cancelar";
+            this._botonCancelarCaja.UseVisualStyleBackColor = true;
+            // 
+            // _botonAgregarCaja
+            // 
+            this._botonAgregarCaja.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._botonAgregarCaja.Location = new System.Drawing.Point(269, 242);
+            this._botonAgregarCaja.Name = "_botonAgregarCaja";
+            this._botonAgregarCaja.Size = new System.Drawing.Size(117, 42);
+            this._botonAgregarCaja.TabIndex = 1;
+            this._botonAgregarCaja.Text = "Agregar";
+            this._botonAgregarCaja.UseVisualStyleBackColor = true;
+            // 
+            // _labelNombreCajero
+            // 
+            this._labelNombreCajero.AutoSize = true;
+            this._labelNombreCajero.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._labelNombreCajero.Location = new System.Drawing.Point(191, 35);
+            this._labelNombreCajero.Name = "_labelNombreCajero";
+            this._labelNombreCajero.Size = new System.Drawing.Size(195, 25);
+            this._labelNombreCajero.TabIndex = 2;
+            this._labelNombreCajero.Text = "Nombre Cajero:";
+            // 
+            // _labelApellidoCajero
+            // 
+            this._labelApellidoCajero.AutoSize = true;
+            this._labelApellidoCajero.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._labelApellidoCajero.Location = new System.Drawing.Point(189, 83);
+            this._labelApellidoCajero.Name = "_labelApellidoCajero";
+            this._labelApellidoCajero.Size = new System.Drawing.Size(197, 25);
+            this._labelApellidoCajero.TabIndex = 3;
+            this._labelApellidoCajero.Text = "Apellido Cajero:";
+            // 
+            // _labelRutCajero
+            // 
+            this._labelRutCajero.AutoSize = true;
+            this._labelRutCajero.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._labelRutCajero.Location = new System.Drawing.Point(237, 136);
+            this._labelRutCajero.Name = "_labelRutCajero";
+            this._labelRutCajero.Size = new System.Drawing.Size(149, 25);
+            this._labelRutCajero.TabIndex = 4;
+            this._labelRutCajero.Text = "RUT Cajero:";
+            // 
+            // _textboxNombreCajero
+            // 
+            this._textboxNombreCajero.Location = new System.Drawing.Point(425, 42);
+            this._textboxNombreCajero.Name = "_textboxNombreCajero";
+            this._textboxNombreCajero.Size = new System.Drawing.Size(100, 20);
+            this._textboxNombreCajero.TabIndex = 5;
+            // 
+            // _textboxApellidoCajero
+            // 
+            this._textboxApellidoCajero.Location = new System.Drawing.Point(425, 90);
+            this._textboxApellidoCajero.Name = "_textboxApellidoCajero";
+            this._textboxApellidoCajero.Size = new System.Drawing.Size(100, 20);
+            this._textboxApellidoCajero.TabIndex = 6;
+            // 
+            // _textboxRutCajero
+            // 
+            this._textboxRutCajero.Location = new System.Drawing.Point(425, 143);
+            this._textboxRutCajero.Name = "_textboxRutCajero";
+            this._textboxRutCajero.Size = new System.Drawing.Size(100, 20);
+            this._textboxRutCajero.TabIndex = 7;
+            // 
+            // _textboxNumCaja
+            // 
+            this._textboxNumCaja.Location = new System.Drawing.Point(425, 186);
+            this._textboxNumCaja.Name = "_textboxNumCaja";
+            this._textboxNumCaja.Size = new System.Drawing.Size(100, 20);
+            this._textboxNumCaja.TabIndex = 8;
+            // 
+            // _labelNumCaja
+            // 
+            this._labelNumCaja.AutoSize = true;
+            this._labelNumCaja.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._labelNumCaja.Location = new System.Drawing.Point(179, 179);
+            this._labelNumCaja.Name = "_labelNumCaja";
+            this._labelNumCaja.Size = new System.Drawing.Size(207, 25);
+            this._labelNumCaja.TabIndex = 9;
+            this._labelNumCaja.Text = "Número de Caja:";
+            // 
+            // _ventanaCrearCaja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(139)))), ((int)(((byte)(12)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._labelNumCaja);
+            this.Controls.Add(this._textboxNumCaja);
+            this.Controls.Add(this._textboxRutCajero);
+            this.Controls.Add(this._textboxApellidoCajero);
+            this.Controls.Add(this._textboxNombreCajero);
+            this.Controls.Add(this._labelRutCajero);
+            this.Controls.Add(this._labelApellidoCajero);
+            this.Controls.Add(this._labelNombreCajero);
+            this.Controls.Add(this._botonAgregarCaja);
+            this.Controls.Add(this._botonCancelarCaja);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "_ventanaCrearCaja";
+            this.Text = "Nueva Caja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _botonCancelarCaja;
+        private System.Windows.Forms.Button _botonAgregarCaja;
+        private System.Windows.Forms.Label _labelNombreCajero;
+        private System.Windows.Forms.Label _labelApellidoCajero;
+        private System.Windows.Forms.Label _labelRutCajero;
+        private System.Windows.Forms.TextBox _textboxNombreCajero;
+        private System.Windows.Forms.TextBox _textboxApellidoCajero;
+        private System.Windows.Forms.TextBox _textboxRutCajero;
+        private System.Windows.Forms.TextBox _textboxNumCaja;
+        private System.Windows.Forms.Label _labelNumCaja;
+    }
+}
