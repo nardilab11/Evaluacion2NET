11412b587e254e64cb6c487fc150aecc93e9f330
Nicolas <nicolas.borjardila11@gmail.com>
2020-06-07 12:54:39 -0400

    Create VentanaCrearPedido.Designer.cs

---
+++ Vista/VentanaCrearPedido.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Evaluacion2NET.Vista
+{
+    partial class _ventanaCrearPedido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_ventanaCrearPedido));
+            this._labelNombreCliente = new System.Windows.Forms.Label();
+            this._labelApellidoCliente = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this._textboxNomCliente = new System.Windows.Forms.TextBox();
+            this._textboxApellCliente = new System.Windows.Forms.TextBox();
+            this._labelDescuento = new System.Windows.Forms.Label();
+            this._labelDespacho = new System.Windows.Forms.Label();
+            this._listboxMenu = new System.Windows.Forms.CheckedListBox();
+            this._textboxDescuento = new System.Windows.Forms.TextBox();
+            this._comboboxDespacho = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this._textboxDirecCliente = new System.Windows.Forms.TextBox();
+            this._botonSiguientePedido = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _labelNombreCliente
+            // 
+            this._labelNombreCliente.AutoSize = true;
+            this._labelNombreCliente.Location = new System.Drawing.Point(48, 50);
+            this._labelNombreCliente.Name = "_labelNombreCliente";
+            this._labelNombreCliente.Size = new System.Drawing.Size(99, 13);
+            this._labelNombreCliente.TabIndex = 0;
+            this._labelNombreCliente.Text = "Nombre del Cliente:";
+            // 
+            // _labelApellidoCliente
+            // 
+            this._labelApellidoCliente.AutoSize = true;
+            this._labelApellidoCliente.Location = new System.Drawing.Point(48, 85);
+            this._labelApellidoCliente.Name = "_labelApellidoCliente";
+            this._labelApellidoCliente.Size = new System.Drawing.Size(99, 13);
+            this._labelApellidoCliente.TabIndex = 1;
+            this._labelApellidoCliente.Text = "Apellido del Cliente:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Menú:";
+            // 
+            // _textboxNomCliente
+            // 
+            this._textboxNomCliente.Location = new System.Drawing.Point(177, 43);
+            this._textboxNomCliente.Name = "_textboxNomCliente";
+            this._textboxNomCliente.Size = new System.Drawing.Size(100, 20);
+            this._textboxNomCliente.TabIndex = 3;
+            // 
+            // _textboxApellCliente
+            // 
+            this._textboxApellCliente.Location = new System.Drawing.Point(177, 78);
+            this._textboxApellCliente.Name = "_textboxApellCliente";
+            this._textboxApellCliente.Size = new System.Drawing.Size(100, 20);
+            this._textboxApellCliente.TabIndex = 4;
+            // 
+            // _labelDescuento
+            // 
+            this._labelDescuento.AutoSize = true;
+            this._labelDescuento.Location = new System.Drawing.Point(338, 43);
+            this._labelDescuento.Name = "_labelDescuento";
+            this._labelDescuento.Size = new System.Drawing.Size(160, 13);
+            this._labelDescuento.TabIndex = 5;
+            this._labelDescuento.Text = "Cupón de Descuento (opcional):";
+            // 
+            // _labelDespacho
+            // 
+            this._labelDespacho.AutoSize = true;
+            this._labelDespacho.Location = new System.Drawing.Point(338, 81);
+            this._labelDespacho.Name = "_labelDespacho";
+            this._labelDespacho.Size = new System.Drawing.Size(120, 13);
+            this._labelDespacho.TabIndex = 6;
+            this._labelDespacho.Text = "¿Despacho a domicilio?";
+            // 
+            // _listboxMenu
+            // 
+            this._listboxMenu.FormattingEnabled = true;
+            this._listboxMenu.Location = new System.Drawing.Point(177, 124);
+            this._listboxMenu.Name = "_listboxMenu";
+            this._listboxMenu.Size = new System.Drawing.Size(120, 94);
+            this._listboxMenu.TabIndex = 7;
+            // 
+            // _textboxDescuento
+            // 
+            this._textboxDescuento.Location = new System.Drawing.Point(524, 40);
+            this._textboxDescuento.Name = "_textboxDescuento";
+            this._textboxDescuento.Size = new System.Drawing.Size(100, 20);
+            this._textboxDescuento.TabIndex = 8;
+            // 
+            // _comboboxDespacho
+            // 
+            this._comboboxDespacho.FormattingEnabled = true;
+            this._comboboxDespacho.Location = new System.Drawing.Point(524, 77);
+            this._comboboxDespacho.Name = "_comboboxDespacho";
+            this._comboboxDespacho.Size = new System.Drawing.Size(121, 21);
+            this._comboboxDespacho.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(338, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Dirección del Cliente (opcional):";
+            // 
+            // _textboxDirecCliente
+            // 
+            this._textboxDirecCliente.Location = new System.Drawing.Point(524, 117);
+            this._textboxDirecCliente.Name = "_textboxDirecCliente";
+            this._textboxDirecCliente.Size = new System.Drawing.Size(100, 20);
+            this._textboxDirecCliente.TabIndex = 11;
+            // 
+            // _botonSiguientePedido
+            // 
+            this._botonSiguientePedido.Location = new System.Drawing.Point(309, 275);
+            this._botonSiguientePedido.Name = "_botonSiguientePedido";
+            this._botonSiguientePedido.Size = new System.Drawing.Size(75, 23);
+            this._botonSiguientePedido.TabIndex = 12;
+            this._botonSiguientePedido.Text = "Siguiente";
+            this._botonSiguientePedido.UseVisualStyleBackColor = true;
+            // 
+            // _ventanaCrearPedido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(139)))), ((int)(((byte)(12)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._botonSiguientePedido);
+            this.Controls.Add(this._textboxDirecCliente);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this._comboboxDespacho);
+            this.Controls.Add(this._textboxDescuento);
+            this.Controls.Add(this._listboxMenu);
+            this.Controls.Add(this._labelDespacho);
+            this.Controls.Add(this._labelDescuento);
+            this.Controls.Add(this._textboxApellCliente);
+            this.Controls.Add(this._textboxNomCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this._labelApellidoCliente);
+            this.Controls.Add(this._labelNombreCliente);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "_ventanaCrearPedido";
+            this.Text = "Nuevo Pedido";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label _labelNombreCliente;
+        private System.Windows.Forms.Label _labelApellidoCliente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox _textboxNomCliente;
+        private System.Windows.Forms.TextBox _textboxApellCliente;
+        private System.Windows.Forms.Label _labelDescuento;
+        private System.Windows.Forms.Label _labelDespacho;
+        private System.Windows.Forms.CheckedListBox _listboxMenu;
+        private System.Windows.Forms.TextBox _textboxDescuento;
+        private System.Windows.Forms.ComboBox _comboboxDespacho;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox _textboxDirecCliente;
+        private System.Windows.Forms.Button _botonSiguientePedido;
+    }
+}
