e43839394126993c7bd5f3a4d9abc6c375ac0bb1
Nicolas <nicolas.borjardila11@gmail.com>
2020-06-24 22:18:45 -0400

    V. 1.0

--- Modelo/Pedido.cs
+++ Modelo/Pedido.cs
@@ -16,14 +16,14 @@ namespace Evaluacion2NET.Modelo
         private Cliente _cliente;
         private Caja _caja;
         private Repartidor _repartidor;
-        private Garzon _garzon;
+        
 
         public Pedido(Cliente cliente, Caja caja)
         {
             this._cliente = cliente;
             this._caja = caja;
             this._repartidor = null;
-            this._garzon = null;
+            
         }
 
         
@@ -31,6 +31,6 @@ namespace Evaluacion2NET.Modelo
         public Cliente Cliente { get => _cliente; set => _cliente = value; }
         public Caja Caja { get => _caja; set => _caja = value; }
         public Repartidor Repartidor { get => _repartidor; set => _repartidor = value; }
-        public Garzon Garzon { get => _garzon; set => _garzon = value; }
+        
     }
 }
